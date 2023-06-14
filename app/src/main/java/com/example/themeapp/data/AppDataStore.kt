04718b47ab936d243a8c928d19ea80d45f657dda
Burak <burak.sahin95@hotmail.com>
2023-06-14 18:56:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/themeapp/data/AppDataStore.kt
@@ -0,0 +1,29 @@
+package com.example.themeapp.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.themeapp.ui.components.data.CustomTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class AppDataStore(private val context: Context) {
+
+    companion object {
+        private val Context.appDataStore: DataStore<Preferences> by preferencesDataStore("preferences")
+        val THEME_KEY = intPreferencesKey("theme")
+    }
+
+    var theme: Flow<CustomTheme> = context.appDataStore.data.map { pref ->
+        CustomTheme.getTheme(pref[THEME_KEY])
+    }
+
+    suspend fun saveTheme(theme: CustomTheme) {
+        context.appDataStore.edit { prefs ->
+            prefs[THEME_KEY] = theme.value
+        }
+    }
+}
